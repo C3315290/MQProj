98eb3e170a54cae190c48e531a2dd9f4d972b2a8
C3315290 <48082264+C3315290@users.noreply.github.com>
2019-04-04 22:26:59 +1100

    Create main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+/* Main function of the C program. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+
+float data[] = {1.2, 8.4, 10.8, -0.4, -8.234, 3.1};
+float maximum = -1e30;
+int maxindex;
+int i;
+
+    for (i = 0; i < 6; i++) {
+        
+        if (data[i] > maximum) {
+        maximum = data[i];
+        maxindex = i;
+    
+        }
+        
+        
+    }
+printf("The maximum value was %f, found at index %d", maximum, maxindex);
+}
