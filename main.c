2192a3b33af81df2ccc50b0a5f1acddf91f183a3
C3315290 <48082264+C3315290@users.noreply.github.com>
2019-04-09 00:28:10 +1000

    1 word working

--- main.c
+++ main.c
@@ -3,15 +3,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void move(char *str, int N);
+
+int strlen(char *str);
+
 int main()
 {
-
-float letters[26]
-float lettercip[26]
-int i;
-
-    for (i = 1; i < 26; i++) {
-        printf("%f", letters[i])
-        }        
+    
+    
+    
+char str[] = "jvumpkluaphs";
+int i = 0;
+while (i < 26) {
+    move(str, strlen(str));
+    printf("%s\n", str);
+    i++;
+}            
+    printf("%s\n", str);
     }
+
+void move(char *str, int N) {
+int i = 0; // Array index loop counter
+    for(i = 0; i < N; i++)
+        if (str[i] != 122) 
+        {
+            str[i] = str[i] + 1; // Use array syntax
+         }
+        else 
+        {
+            str[i] = 97;
+        }
+        
+return; // Optional
 }
+
+
+int strlen(char *str) {
+    int i = 0;
+    while (str[i] != 0) {
+    
+     i++;
+ }
+     return i;
+    }
+
