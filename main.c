76f6f99d5679ab590b4476ac76d3e3cf7c9d9f8e
C3315290 <48082264+C3315290@users.noreply.github.com>
2019-04-09 23:26:43 +1000

    Multiple words, 26 permutations

--- main.c
+++ main.c
@@ -4,39 +4,38 @@
 #include <stdlib.h>
 
 void move(char *str, int N);
-
 int strlen(char *str);
 
 int main()
 {
     
-    
-    
-char str[] = "jvumpkluaphs";
+char str[] = "pm ol ohk hufaopun jvumpkluaphs av zhf, ol dyval pa pu jpwoly, aoha pz, if zv johunpun aol vykly vm aol slaalyz vm aol hswohila, aoha uva h dvyk jvbsk il thkl vba.";
+
 int i = 0;
+
+
 while (i < 26) {
     move(str, strlen(str));
-    printf("%s\n", str);
+    printf("%d: %s\n",i, str);
     i++;
 }            
-    printf("%s\n", str);
     }
-
+    
 void move(char *str, int N) {
 int i = 0; // Array index loop counter
-    for(i = 0; i < N; i++)
-        if (str[i] != 122) 
-        {
-            str[i] = str[i] + 1; // Use array syntax
-         }
-        else 
-        {
-            str[i] = 97;
+
+    for(i = 0; i < N; i++) {
+        if (str[i] != 32 && str[i] >= 97 && str[i] <= 122) {
+        if (str[i] == 122) {
+            str[i] = 97; 
         }
-        
+            else {
+                str[i] = str[i] + 1;
+            }
+    }
+}
 return; // Optional
 }
-
 
 int strlen(char *str) {
     int i = 0;
