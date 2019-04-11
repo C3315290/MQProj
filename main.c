1c18041665bb92132883a6eb6ff72b9f7f6c0d23
C3315290 <48082264+C3315290@users.noreply.github.com>
2019-04-11 17:49:05 +1000

    split first word

--- main.c
+++ main.c
@@ -4,26 +4,58 @@
 #include <stdlib.h>
 
 
-int strlen(char *str);
+int len(char *str);
 void move(char *str, int N);
 
 int main()
 {
     
 char str[] = "pm ol ohk hufaopun jvumpkluaphs av zhf, ol dyval pa pu jpwoly, aoha pz, if zv johunpun aol vykly vm aol slaalyz vm aol hswohila, aoha uva h dvyk jvbsk il thkl vba.";
-char longest[1500];
-int i = 0;
+char first[1500];
+char secoind[1500];
+int spacessec = 0;
+char third [1500];
 
-FILE *input;
+int i = 0;
+int count = 0;
+int words = 0;
+
+
+// Returns first word
+while (count < len(str) && words < 1) {
+    if (str[count] != ' ') {
+       first[count] = str[count];
+       count++;
+   } 
+    else {
+        words++;
+    }
+}
+printf("%s\n", first);
+
+// Returns seond word
+while (count < len(str) && words < 1) {
+    if (str[count] = ' '){
+       spacessec++;
+    if (spacessec = 1) {;
+
+    }
+    count++;
+}
+printf("%s\n", first);
+
+
+
+// FILE *input;
 FILE *output;
 
-input = fopen("words.txt", "r");
+// input = fopen("words.txt", "r");
 output = fopen("ciph.txt", "w");
 
 fprintf(output, "%s", str);
 
 while (i < 26) {
-    move(str, strlen(str));
+    move(str, len(str));
     printf("%d: %s\n",i, str);
     i++;
 }            
@@ -45,7 +77,7 @@ int i = 0; // Array index loop counter
 return;
 }
 
-int strlen(char *str) {
+int len(char *str) {
     int i = 0;
     while (str[i] != 0) {
     
