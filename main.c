37e4e2e10e96e3b813e50a4f78f55e94971f7df4
Matt Quast <C3315290@uon.edu.au>
2019-04-27 01:10:30 +0000

    Indentation Done, ceaser only

--- main.c
+++ main.c
@@ -3,100 +3,170 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-void move(char *str, int N);
+void demove(char *str, int N);
+void enmove(char *encyptstr, int N);
 void returnword(char *str, char *word, int wordnumber, int N);
-void init(char *word);  //sets entire word string to '\0' ensures when function returnword is run, only a word returns.
+void initword(char *word);  //sets entire word string to '\0' ensures when function returnword is run, only a word returns.
 int nowords(char *str, int N);
 int containspunc(char *word, int K);
+void initfind(char *find);
+void upperstr(char *str);
+void upperenstr(char *encyptstr);
 
 
 int main()
 {
     
-char str[] = "pc tktg-vgdlxcv cjbqtg du rdbeatm pcs gxvxs gjath eajh wpgs-id-rdet-lxiw gtvjapixdch pgt cdl qtxcv atvxhapits ugdb hipit id hipit. ztn utstgpa gtvjapixdch ltgt udgbjapits qn iwt usp, uir, pcs iwt rehr. tprw du iwtht utstgpa pvtcrxth htgkth p hetrxuxr bxhhxdc. dct tmpbeat: aplh hedchdgts qn iwt duuxrt du iwt upxg stqi rdaatrixdc egprixrth egtktci pc pvtcrn ugdb ejgedhtujaan wpgphhxcv raxtcih xc htgxdjh stqi. iwt upxg eprzpvxcv pcs apqtaxcv pri bpzth rtgipxc iwpi egditrixdc ugdb bxhatpsxcv eprzpvxcv du vddsh xh vjpgpcitts id tprw qjntg du vddsh rpggxts xc hbpaa hwdeh ph ltaa ph xc apgvt hjetgbpgztih. egdsjrih dc iwt bpgzti bjhi gtktpa iwt cpbth du paa xcvgtsxtcih dc iwt apqta. apcvjpvt bjhi qt xc ratpg pcs egtrxht itgbh iwpi rpc qt jcstghidds qn tktgndct. iwxh egprixrt xh ktgn rgjrxpa udg iwt axkth du bpcn etdeat. xi xh egjstci iwpi lt gtrpaa iwpi iwt usp hetrxuxrpaan gtfjxgth iwpi paa vddsh pgt ejgt, hput, pcs lwdathdbt. iwt usp hipith iwpi paa vddsh qt egdsjrts jcstg wxvwan hpcxipgn rdcsxixdch. sgjvh bjhi qt rdbeatitan hput pcs bjhi pahd qt tuutrixkt udg iwtxg hipits ejgedht. iwxh edaxrn peeaxth id rdhbtixrh iwpi bjhi qt qdiw hput pcs ejgt. xcsxkxsjpah pgt duitc idipaan jcpeegtrxpixkt du iwt usp'h vgtpi stsxrpixdc.";
-int i = 0;
-int k = 0;
+char str[100000];
+char encyptstr[100000];
+int j = 1;
+int k = 1;
 int l = 1;
+int o = 1;
 int largestwordpos = 1;
 int largestwordlength = 1;
 char word[100];
 char find[1000];
 int cipherresult = 0;
+int ciphermove;
+char c;
 
 
-init(word);
-init(find);
+printf("Please select an option: \n");
+printf("a) Encrypt ceaser cypher, place test to be encrypted in Cipher.txt, enter a key.\n");
+printf("b) Decrypt ceaser cypher without key\n");
+printf("c) Decrypt ceaser cypher given Key\n");
+printf("Selection: ");
+scanf("%c", &c);
 
-FILE *input;
-input = fopen("words.txt", "r");
+initword(word);
+initfind(find);
 
-while (l <= nowords(str, strlen(str))) {
-    returnword(str, word, l, strlen(str));
-    
+FILE *dict;
+dict = fopen("words.txt", "r");
+FILE *output;
+output = fopen("decrypted.txt", "w");
+FILE *encryptf;
+encryptf = fopen("cipherencrypt.txt", "r");
+FILE *decryptf;
+decryptf  = fopen("cipherdecrypt.txt", "r");
 
-    if (strlen(word) > largestwordlength && containspunc(word, strlen(word)) == 0) {
-        largestwordlength = strlen(word);
-        largestwordpos = l;    
+switch (c) {
+ 
+case 'a':
+printf("Enter Cypher Movement Integer:");
+scanf("%d", &ciphermove);
+rewind(encryptf) ;
+        while(fscanf(encryptf, "%[^\n]", encyptstr) != EOF){
+            upperenstr(encyptstr);
+            while (o <= ciphermove) {
+                enmove(encyptstr, strlen(encyptstr)); 
+                o++;
+            }     
+        }
+printf("Encrypted text with movement of %d: %s\n", ciphermove, encyptstr);  
+break;
+
+case 'b':
+
+while(fscanf(decryptf, "%[^\n]", str) != EOF){
+    upperenstr(str);
+    while (l <= nowords(str, strlen(str))) {
+        returnword(str, word, l, strlen(str));
+        if (strlen(word) > largestwordlength && containspunc(word, strlen(word)) == 0) {
+            largestwordlength = strlen(word);
+            largestwordpos = l;    
+        }
+    l++;
     }
-
-l++;
 }
-
 returnword(str, word, largestwordpos, strlen(str));
 printf("Word used for spell checking: %s\n", word);
 
-
-while (i < 26) {
-    move(word, strlen(word));
-rewind(input);
-while(fscanf(input, "%s", find) != EOF)
-{
-	if(strcmp(find, word) == 0)
-	{
-		cipherresult = i;
-		printf("Word Found: %s\n", find);
-	}
+while (j < 26) {
+    demove(word, strlen(word));
+    rewind(dict);
+    while(fscanf(dict, "%s", find) != EOF){
+        if(strcmp(find, word) == 0){
+		  cipherresult = j;
+		  printf("Word Found: %s\n", find);
+	   }
+    }
+    j++;
 }
-    i++;
-}
-printf("Cipher Movement: %d\n", cipherresult);
+printf("Cipher Movement: %d\n", abs(cipherresult - 26));
 
 while (k < 26) {
-    move(str, strlen(str));
+    demove(str, strlen(str));
     if (k == cipherresult) {
         printf("Decrypted: %s\n", str);
+        fprintf(output, "%s", str);
     }
-     k++;
+    k++;
+}
+break;
+case 'c':
+
+printf("Enter Cypher Movement Integer:");
+scanf("%d", &ciphermove);
+
+while(fscanf(decryptf, "%[^\n]", str) != EOF){
+    upperenstr(str);
+    while (k < 26) {
+        demove(str, strlen(str));
+        if (k == abs(ciphermove - 26)) {
+            printf("Decrypted: %s\n", str);
+            fprintf(output, "%s", str);
+        }
+        k++;
+    }
 }
 
+break;
+default: printf("Unknown option %c\nPlease enter a or b\n",c);
+}
 }
 
 // ------------------------------FUNCTIONS--------------------------------------------
-void move(char *str, int N) {
+void demove(char *str, int N) {
 int i = 0; // Array index loop counter
     for(i = 0; i < N; i++) {
-        if (str[i] != 32 && str[i] >= 97 && str[i] <= 122) {
-        if (str[i] == 122) {
-            str[i] = 97; 
+        if (str[i] != 32 && str[i] >= 65 && str[i] <= 90) {
+        if (str[i] == 90) {
+            str[i] = 65; 
         }
             else {
-                str[i] = str[i] + 1;
+                str[i] = str[i] +1;
             }
     }
 }
 return;
 }
+
+void enmove(char *encyptstr, int N) {
+int i = 0; // Array index loop counter
+    for(i = 0; i < N; i++) {
+        if ((encyptstr[i] >= 65 && encyptstr[i] <= 90)) {
+            if (encyptstr[i] == 90) {
+                encyptstr[i] = 65; 
+            }
+            else {
+                encyptstr[i] = encyptstr[i] + 1;
+            }
+        }
+    }
+return;
+}
     
-void returnword(char *str, char *word, int n, int N) 
-{
+    
+void returnword(char *str, char *word, int n, int N) {
     int count = 0;
     int count2 = 0;
     int spaces = 0;
-    if (n == 1)
-   {
-        while (str[count] != ' ') 
-        {
+    if (n == 1){
+        while (str[count] != ' ') {
         word[count] = str[count];       
         count++;
         }
@@ -112,57 +182,73 @@ void returnword(char *str, char *word, int n, int N)
             count2++;
                     if (str[count] == ' ') {
                     spaces++;
-                    if (spaces == n) {
-                           word[count2] = '\0';
+                        if (spaces == n) {
+                               word[count2] = '\0';
+                        }
                     }
-                }
             }
-                if (str[count] == ' ') {
+            if (str[count] == ' ') {
                     spaces++;
-                }
-
-            count++;
             }
+            count++;
+        }
     }
     return;
 }
 
-void init(char *word) {
+void initword(char *word) {
     int i = 0;
-
     while (word[i] != '\0') {
-                word[i] = '\0';
-                i++;
-                }
-    }  
-    int nowords(char *str, int N) {
-        int j = 0;
-        int words = 0;
-        for (j = 0; j < N; j++) {
-        if (str[j] == ' ') {
-        words++;   
-                    ;
-        }
+        word[i] = '\0';
+        i++;
+    }
+}      
+    
+void upperenstr(char *encyptstr) {
+    int i = 0;
+    while (encyptstr[i] != '\0') {
+        encyptstr[i] = (toupper(encyptstr[i]));
+        i++;
+    }
+}  
+
+void upperstr(char *str) {
+    int i = 0;
+    while (str[i] != '\0') {
+        str[i] = (toupper(str[i]));
+        i++;
+    }
+}  
+
+void initfind(char *find) {
+    int i = 0;
+    while (find[i] != '\0') {
+        find[i] = '\0';
+        i++;
+    }
 }
-        return words + 1;
-    }
-    
-    
-    int containspunc(char *word, int K) {
-        int j = 0;
-        int r = 0;
-        while (j <= K) {
-
-                    if ((word[j] >= 33 && word[j] <= 64) || (word[j] >= 91 && word[j] <= 96) || (word[j] >= 123 && word[j] <= 126)) {
-                    r = 1;
-                    }
-        j++;
+  
+int nowords(char *str, int N) {
+    int j = 0;
+    int words = 0;
+    for (j = 0; j < N; j++) {
+        if (str[j] == ' ') {
+            words++;   
         }
-        return r;
     }
-
-
-
-
-                    
-                    
+return words + 1;
+}
+    
+    
+int containspunc(char *word, int K) {
+    int j = 0;
+    int r = 0;
+    while (j <= K) {
+        if ((word[j] >= 33 && word[j] <= 64) || (word[j] >= 91 && word[j] <= 96) || (word[j] >= 123 && word[j] <= 126)) {
+        r = 1;
+        }
+    j++;
+    }
+return r;
+}
+           
